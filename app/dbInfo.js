b8f65862c68e6ae43febc31a7942b9cb634f624b
Reuben Chouraki <chourakirgit@yahoo.com>
2020-10-12 11:00:07 +0200

    new file modified

--- app/dbInfo.js
+++ app/dbInfo.js
@@ -1,4 +1,8 @@
-const { Client } = require("pg");
-const client = new Client(process.env.TESTDB);
-client.connect();
-module.exports = client;
+try {
+  const { Client } = require("pg");
+  const client = new Client(process.env.PROD_DB);
+  client.connect();
+  module.exports = client;
+} catch (error) {
+  console.log("an error occured here");
+}
